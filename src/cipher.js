e324202bb4ef41afb618109a7ae69419ff1f841e
Karina Pereira de Souza <karina.souza1981@gmail.com>
2020-03-31 12:55:20 -0300

    funcao enconde e decode do cipher.js cifra de cesar

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,14 @@
+    // , '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' '
+
 const cipher = {
   encode: function(numero, texto) {
     numero = parseInt(numero);
-    let caracteresValidos = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const caracteresValidos = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', ' '];
 
     let resultado = '';
-    let myRe = /[^A-Z-0-9]+/g;
+    let myRe = /[^A-Z\ ]+/g;
     let verificaCaracteresValidos = texto.match(myRe);
-    
+
     if (verificaCaracteresValidos && verificaCaracteresValidos.length > 0) {
       alert('Voce deve digitar apenas letras de A - Z e numeros')
       return false;
@@ -23,6 +25,7 @@ const cipher = {
         resultado += caracteresValidos[indice].toString();
       }
       
+      return resultado;
 
     } else {
       alert('Informe um numero maior que zero no campo solicitado.');
@@ -32,6 +35,40 @@ const cipher = {
 
   },
   decode: function(numero, texto) {
+    numero = parseInt(numero);
+    const caracteresValidos = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', ' '];
+
+    let resultado = '';
+    let myRe = /[^A-Z\ ]+/g;
+    let verificaCaracteresValidos = texto.match(myRe);
+
+    if (verificaCaracteresValidos && verificaCaracteresValidos.length > 0) {
+      alert('Voce deve digitar apenas letras de A - Z e numeros')
+      return false;
+    }
+    if (numero != NaN && numero > 0) {
+
+      for(let contador = 0; contador < texto.length; contador++) {
+        let caracterAtual = texto[contador];
+        let indexValido = caracteresValidos.indexOf(caracterAtual);
+        let contadorCifrado = indexValido - numero;
+
+        let indice = contadorCifrado % caracteresValidos.length;
+
+        if (indice < 0) {
+          indice = caracteresValidos.length + indice;
+        }
+
+        resultado += caracteresValidos[indice].toString();
+      }
+      
+      return resultado;
+
+    } else {
+      alert('Informe um numero maior que zero no campo solicitado.');
+
+      return false;
+    }
 
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import cipher from './cipher.js';
 
+window.cipher = cipher;
+
 console.log(cipher);
+
+let numero = document.getElementById('password');
+
