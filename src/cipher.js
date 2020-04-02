69eda3946dff3e74c80adbe5a8934373c8d3693c
Karina Pereira de Souza <karina.souza1981@gmail.com>
2020-04-02 12:39:16 -0300

    ajustes

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,12 @@
 
 const cipher = {
-  caracteresValidos: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+  // caracteresValidos: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
   encode: function(numero, texto) {
     if (!numero || !texto) {
       throw new TypeError('Parametros requiridos', 2);
     }
     texto = texto.toUpperCase();
-    const caracteresValidos = this.caracteresValidos;
+    const caracteresValidos = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
     let resultado = "";
     let myRe = /[^A-Z]+/g;
@@ -38,7 +38,7 @@ const cipher = {
     }
 
     texto = texto.toUpperCase();
-    const caracteresValidos = this.caracteresValidos;
+    const caracteresValidos = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
     let resultado = '';
     let myRe = /[^A-Z]+/g;
