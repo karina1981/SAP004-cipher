e3b6bfd3587a99db64fe4bc8b8cd64b114250475
Karina Pereira de Souza <karina.souza1981@gmail.com>
2020-04-07 11:21:50 -0300

    arrumado o css, js e html

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,25 @@
-
 const cipher = {
   encode: function(numero, texto) {
-    
+    texto = texto.toUpperCase();//.replace(/[^A-Z]+/g,"");
+    numero = parseInt(numero);
+    let resultado = "";
+    for(let contador = 0; contador < texto.length; contador++) {
+      let indexValido = (((texto.charCodeAt(contador) - 65) + numero)%26 )+ 65;
+      resultado += String.fromCharCode(indexValido);
+    }
+    return resultado;
   },
   decode: function(numero, texto) {
-  
+    texto = texto.toUpperCase();//.replace(/[^A-Z]+/g,"");
+    numero = parseInt(numero);
+    let resultado = "";
+    for(let contador = 0; contador < texto.length; contador++){
+      let indexValido = (((texto.charCodeAt(contador) - 90) - numero)% 26) + 90;
+      resultado += String.fromCharCode(indexValido);
+    }
+    return resultado;
   }
 };
 
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import cipher from './cipher.js';
 
+document.getElementById('crip').addEventListener('click', function(){
+    let codigo = document.getElementById('codigo').value
+    let text = document.querySelector('textarea[name="textOn"]').value
+     //executar uma função dentro do cipher
+    document.getElementById('resultado').innerHTML=cipher.encode(codigo, text);
+    console.log(codigo, text);
+})
+
+document.getElementById('decrip').addEventListener('click', function(){
+    let codigo = document.getElementById('codigo').value
+    let text = document.querySelector('textarea[name="textOn"]').value
+
+    document.getElementById('resultado').innerHTML=cipher.decode(codigo, text);
+
+    console.log(codigo, text);
+})
